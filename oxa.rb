938b1e96245c47a10fb105e2461201c9cf3ff580
Faisal <faisal@mvp-apps.ae>
2025-03-29 17:06:21 +0400

    Bump Oxa version to 1.0.2 and update installation script

--- oxa.rb
+++ oxa.rb
@@ -2,11 +2,11 @@ class Oxa < Formula
   desc "Oxa CLI - A lightweight command-line tool"
   homepage "https://github.com/Faisalkc4u/homebrew-oxa"
   url "https://github.com/Faisalkc4u/homebrew-oxa/archive/refs/heads/main.zip"
-  version "1.0.1"
+  version "1.0.2"
 
   def install
     # Install the upgrade script
-    bin.install "upgrade.sh" => "oxa-upgrade.sh"
+    bin.install "oxa.sh" => "oxa"
   end
 
   test do
