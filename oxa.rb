0b0f5638ba85c2a2292c53f2a59e71df33a475a9
Faisal <faisal@mvp-apps.ae>
2025-03-29 16:22:47 +0400

    Update Oxa formula URL to point to the raw GitHub content

--- oxa.rb
+++ oxa.rb
@@ -1,7 +1,7 @@
 class Oxa < Formula
   desc "A powerful CLI tool for Mac"
   homepage "https://github.com/Faisalkc4u/homebrew-oxa"
-  url "https://github.com/Faisalkc4u/homebrew-oxa/releases/download/v1.0.0/oxa.sh" # Update this with the correct file URL
+  url "https://raw.githubusercontent.com/Faisalkc4u/homebrew-oxa/main/oxa.sh" # Replace "main" with your branch name if different
   version "1.0.0"
   sha256 "2bea2d52cd3dff22c9f21b5132aaf735c9c54aff646323b0e0d67a8913cd77a8"
 
