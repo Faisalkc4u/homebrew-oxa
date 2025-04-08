155ac02de594f3730d180f04aa3cba779e846dd5
Faisal <faisal@mvp-apps.ae>
2025-04-08 13:21:41 +0400

    Refactor build function to focus on Firebase distribution

--- build.sh
+++ build.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 # Function to build Flutter app
-build_flutter() {
-    if [ "$1" == "adhoc" ]; then
-        echo "Building Flutter app for AdHoc distribution..."
-        flutter build ipa --export-options-plist=ExportOptionsAdHoc.plist
-    elif [ "$1" == "appstore" ]; then
-        echo "Building Flutter app for App Store distribution..."
-        flutter build ipa --export-options-plist=ExportOptionsAppStore.plist
-    else
-        echo "Invalid argument. Use 'adhoc' or 'appstore'."
-        exit 1
-    fi
+build_firebase() {
+    flutter build ipa --release --export-method=ad-hoc
 }
 
-# Example usage
-# Uncomment the line below to test the function
-# build_flutter adhoc
-# build_flutter appstore
+ 
--- oxa.rb
+++ oxa.rb
@@ -2,7 +2,7 @@ class Oxa < Formula
   desc "Oxa CLI - A lightweight command-line tool"
   homepage "https://github.com/Faisalkc4u/homebrew-oxa"
   url "https://github.com/Faisalkc4u/homebrew-oxa/archive/refs/heads/main.zip"
-  version "1.0.2"
+  version "1.0.3"
 
   def install
     # Install the upgrade script
